6f98bb7c65c8fdd35474f338cc2ea276d21b6c59
Victor Hernandez <20.victor.hernandez@ksd.org>
2019-10-23 12:03:06 -0700

    adding the top down assesment project

---
+++ TopDownViewGame/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int lives = 5;
+    public void StartGame()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("LevelOne");
+        PlayerPrefs.SetInt("lives", lives);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
